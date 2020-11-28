a9787c08b2b6840ca2a30d9b717f218064955353
Gary Sentosa <gary.sentosa@gmail.com>
2020-11-28 14:33:25 +0900

    Move display related methods inside class

--- app.js
+++ app.js
@@ -6,21 +6,23 @@ const Oled = require('oled-js');
 
 const WeatherLed = require('./lib/led');
 const WeatherScraper =require('./lib/scraper');
-const {generateOledFont} = require('./lib/display');
+const OledDisplay = require('./lib/display');
 
 const updateInterval = 60 * 60 * 1000; // milliseconds
 const font = 'k8x12';
+const oledHeight = 64;
 
 registerFont('./font/k8x12.ttf', {family: font});
 
 function main() {
   const board = new five.Board();
   const weatherScraper = new WeatherScraper();
+  const oledDisplay = new OledDisplay();
 
   board.on('ready', async () => {
     const opts = {
       width: 128,
-      height: 64,
+      height: oledHeight,
       address: 0x3C,
     };
 
@@ -45,7 +47,7 @@ function main() {
       const weather = summary.forecasts.today.weather;
 
       weatherLed.operateWeatherLeds(weather);
-      oledFont = generateOledFont(font, 12, [weather]);
+      oledFont = oledDisplay.generateOledFont(font, 12, [weather]);
 
       oled.setCursor(1, 1);
       oled.writeString(oledFont, 1, weather, 1, true, 2);
--- test/display-test.js
+++ test/display-test.js
@@ -1,53 +1,57 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const {calculateOledMaxRow, calculateOledTopMargin} = require('../lib/display');
+const OledDisplay = require('../lib/display');
 
-describe('calculateOledMaxRow()', function() {
-  it('should return 0 if OLED height < text height', function() {
-    const output = calculateOledMaxRow(
-        oledHeight=16,
-        fontHeight=24,
-        textRowCount=1,
-    );
-    expect(output).to.equal(0);
+describe('OledDisplay', function() {
+  const oledDisplay = new OledDisplay();
+
+  describe('calculateOledMaxRow()', function() {
+    it('should return 0 if OLED height < text height', function() {
+      const output = oledDisplay._calculateOledMaxRow(
+          oledHeight=16,
+          fontHeight=24,
+          textRowCount=1,
+      );
+      expect(output).to.equal(0);
+    });
+
+    it('should return 1 if OLED height 1~1.9  text height', function() {
+      const output = oledDisplay._calculateOledMaxRow(
+          oledHeight=23,
+          fontHeight=12,
+          textRowCount=1,
+      );
+      expect(output).to.equal(1);
+    });
+
+    it('should return 4 if OLED height 4x text height', function() {
+      const output = oledDisplay._calculateOledMaxRow(
+          oledHeight=64,
+          fontHeight=12,
+          textRowCount=4,
+      );
+      expect(output).to.equal(4);
+    });
+
+    it('should reserve 1 row for page number', function() {
+      const output = oledDisplay._calculateOledMaxRow(
+          oledHeight=64,
+          fontHeight=12,
+          textRowCount=5,
+      );
+      expect(output).to.equal(3);
+    });
   });
 
-  it('should return 1 if OLED height 1~1.9  text height', function() {
-    const output = calculateOledMaxRow(
-        oledHeight=23,
-        fontHeight=12,
-        textRowCount=1,
-    );
-    expect(output).to.equal(1);
-  });
-
-  it('should return 4 if OLED height 4x text height', function() {
-    const output = calculateOledMaxRow(
-        oledHeight=64,
-        fontHeight=12,
-        textRowCount=4,
-    );
-    expect(output).to.equal(4);
-  });
-
-  it('should reserve 1 row for page number', function() {
-    const output = calculateOledMaxRow(
-        oledHeight=64,
-        fontHeight=12,
-        textRowCount=5,
-    );
-    expect(output).to.equal(3);
-  });
-});
-
-describe('calculateOledTopMargin()', function() {
-  it('should return val which center displayed text', function() {
-    const output = calculateOledTopMargin(
-        oledHeight=64,
-        fontHeight=12,
-        oledMaxRow=4,
-    );
-    expect(output).to.equal(7);
+  describe('calculateOledTopMargin()', function() {
+    it('should return val which center displayed text', function() {
+      const output = oledDisplay._calculateOledTopMargin(
+          oledHeight=64,
+          fontHeight=12,
+          oledMaxRow=4,
+      );
+      expect(output).to.equal(7);
+    });
   });
 });
